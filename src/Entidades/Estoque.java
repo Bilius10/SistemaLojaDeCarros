b6976211311062cd0787dde2afd675a13a0bdec4
João Vitor <jvdrdo@gmail.com>
2024-09-26 22:01:03 -0300

    Update Estoque.java

--- src/Entidades/Estoque.java
+++ src/Entidades/Estoque.java
@@ -18,43 +18,67 @@ public class Estoque {
         this.modelo = modelo;
     }
 
+    public Estoque() {
+    }
+
     public Date getAno() {
+
         return ano;
     }
 
     public void setAno(Date ano) {
+
         this.ano = ano;
     }
 
     public String getCor() {
+
         return cor;
     }
 
     public void setCor(String cor) {
+
         this.cor = cor;
     }
 
     public int getIdEstoque() {
+
         return idEstoque;
     }
 
     public void setIdEstoque(int idEstoque) {
+
         this.idEstoque = idEstoque;
     }
 
     public String getMarca() {
+
         return marca;
     }
 
     public void setMarca(String marca) {
+
         this.marca = marca;
     }
 
     public String getModelo() {
+
         return modelo;
     }
 
     public void setModelo(String modelo) {
+
         this.modelo = modelo;
+    }
+
+    @Override
+    public String toString() {
+        return "Estoque{" +
+                "idEstoque=" + idEstoque +
+                ", marca='" + marca + '\'' +
+                ", modelo='" + modelo + '\'' +
+                ", ano=" + ano +
+                ", cor='" + cor + '\'' +
+                '}';
     }
 }
