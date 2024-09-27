8703d04fd6e7ecb910ce6ec9224c147ef285887b
João Vitor <jvdrdo@gmail.com>
2024-09-26 22:01:22 -0300

    Update Funcionario.java

--- src/Entidades/Funcionario.java
+++ src/Entidades/Funcionario.java
@@ -1,68 +1,80 @@
 package Entidades;
 
-public class Funcionario {
+import java.util.Date;
+
+public class Funcionario extends Pessoa {
 
     private int idFuncionario;
-    private Pessoa pessoa_idpessoa;
     private String cargo;
     private double salario;
     private String setor;
     private int status;
 
-    public Funcionario(String cargo, int idFuncionario, Pessoa pessoa_idpessoa, double salario, String setor) {
+    public Funcionario(String cpf, Date data_nascimento, String email, int idPessoa, String nome, String telefone, String cargo, int idFuncionario, double salario, String setor) {
+        super(cpf, data_nascimento, email, idPessoa, nome, telefone);
         this.cargo = cargo;
         this.idFuncionario = idFuncionario;
-        this.pessoa_idpessoa = pessoa_idpessoa;
         this.salario = salario;
         this.setor = setor;
         this.status = 1;
     }
 
+    public Funcionario() {
+        this.status = 1;
+    }
+
     public String getCargo() {
+
         return cargo;
     }
 
     public void setCargo(String cargo) {
+
         this.cargo = cargo;
     }
 
     public int getIdFuncionario() {
+
         return idFuncionario;
     }
 
     public void setIdFuncionario(int idFuncionario) {
+
         this.idFuncionario = idFuncionario;
     }
 
-    public Pessoa getPessoa_idpessoa() {
-        return pessoa_idpessoa;
-    }
-
-    public void setPessoa_idpessoa(Pessoa pessoa_idpessoa) {
-        this.pessoa_idpessoa = pessoa_idpessoa;
-    }
-
     public double getSalario() {
+
         return salario;
     }
 
     public void setSalario(double salario) {
+
         this.salario = salario;
     }
 
     public String getSetor() {
+
         return setor;
     }
 
     public void setSetor(String setor) {
+
         this.setor = setor;
     }
 
     public int getStatus() {
+
         return status;
     }
 
     public void setStatus(int status) {
+
         this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
     }
 }
